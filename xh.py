accd02f3c831daf06d66929a165815ad6891e710
Mark Fickett <mark.fickett@gmail.com>
2012-06-08 00:36:06 -0400

    Add packing helper and various testing commands.

--- xh.py
+++ xh.py
@@ -46,6 +46,18 @@ def pickSerialDevice():
 		i = 0
 	return serialDevices[i]
 
+def packNumberInString(n):
+	"""
+	Pack numbers of arbitrary size into (little-endian) strings.
+	Example: 0x3ef7 => '\x3e\xf7'
+	"""
+	s = ''
+	while n > 0:
+		lowByte = n % 0x100
+		s = chr(lowByte) + s
+		n = n / 0x100
+	return s
+
 log.info('started')
 log.info('Type control-C to exit.')
 
@@ -58,10 +70,29 @@ try:
 	s = serial.Serial(serialDevice, Config.SERIAL_BAUD)
 	xb = xbee.XBee(s, callback=logData)
 	log.info('Created XBee object.')
-	for i, cmd in enumerate(['MY', 'ID']):
+	for i, cmd in enumerate([
+		#'MY', # node's network ID (0 for coordinator)
+		'ID', # network ID
+		#('ID', '\x3E\xF7'), # set network ID to 0x3EF7
+		#('KY', '\x32\x10'), # set network key to 0x3210
+		#('KY', packNumberInString(Config.LINK_KEY)),
+		#'WR', # write network key
+		#'EE', # encryption enable (0 or 1)
+		#'SH', # serial (high bits)
+		#'SL', # serial (low bits)
+		#'NI', # string node name
+		'%V', # Vcc voltage, value * 1200/1024.0 = mV
+		'NT', # discovery timeout
+		'ND'] # node discover
+	):
 		frameId = '%X' % i
-		log.info('Sending %s with frame ID %s' % (cmd, frameId))
-		xb.at(command=cmd, frame_id=frameId)
+		parameter = None
+		if type(cmd) is tuple:
+			cmd, parameter = cmd
+		log.info('Sending %s with frame ID %s%s' %
+			(cmd, frameId,
+			(parameter and ' and parameter %s' % parameter) or ''))
+		xb.at(command=cmd, frame_id=frameId, parameter=parameter)
 	while True:
 		time.sleep(0.02)
 except KeyboardInterrupt, e:
