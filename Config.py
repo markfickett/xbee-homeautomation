9113a9553695157793d3cdbdf2feaffc2450fba3
Mark Fickett <mark.fickett@gmail.com>
2012-06-08 00:35:49 -0400

    Include values from SecretConfig (link encryption key).

--- Config.py
+++ Config.py
@@ -3,3 +3,6 @@ Configuration constants (shared, not expected to change).
 """
 
 SERIAL_BAUD = 9600
+
+# Link encryption key, etc. Values not to check into version control.
+from SecretConfig import *
