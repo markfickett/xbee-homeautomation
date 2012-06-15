79ee4c4afb73900aa68e86b7e1b037a8d0dbd180
Mark Fickett <mark.fickett@gmail.com>
2012-06-14 22:49:49 -0400

    Add parsing strings as numbers to Encoding.

--- Encoding.py
+++ Encoding.py
@@ -2,6 +2,8 @@
 Conversion functions to pack/unpack values for XBee API communication.
 """
 
+BYTE_BASE = 0x100
+
 def NumberToString(n):
 	"""
 	Pack numbers of arbitrary size into (little-endian) strings.
@@ -9,7 +11,29 @@ def NumberToString(n):
 	"""
 	s = ''
 	while n > 0:
-		lowByte = n % 0x100
+		lowByte = n % BYTE_BASE
 		s = chr(lowByte) + s
-		n = n / 0x100
+		n = n / BYTE_BASE
 	return s
+
+def PrintedStringToNumber(s):
+	"""
+	Unpack a %s-formatted number. (Try to return an int, then a float.)
+	Example: '3' => 3 or '2.2' => 2.2
+	"""
+	try:
+		return int(s)
+	except ValueError, e:
+		return float(s)
+
+def StringToNumber(s):
+	"""
+	Unpack a (little-endian) string to a number.
+	Example: '>\xf7' => 0x3ef7 or '\n\xe4' => 2788 (0x0ae4)
+	"""
+	n = 0
+	for c in s:
+		n *= BYTE_BASE
+		n += ord(c)
+	return n
+
