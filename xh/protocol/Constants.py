f811af5eb721469c2a1ac3164c1a67cf57313ef9
Mark Fickett <mark.fickett@gmail.com>
2012-08-05 21:57:09 -0400

    Add constants for pin names, and map for pin names to numbers.

--- xh/protocol/Constants.py
+++ xh/protocol/Constants.py
@@ -1,5 +1,9 @@
+
 __all__ = [
 	'DEVICE_TYPE',
+
+	'PINS',
+	'PIN_NAMES_TO_NUMBERS',
 ]
 
 from ..deps import Enum
@@ -11,3 +15,92 @@ DEVICE_TYPE = Enum(
 	'END_DEVICE',	# 2
 )
 
+
+# See Xbee Series 2 datasheet page 13 for pin name/number table.
+PINS = Enum(
+	# Digital I/O Pins
+	'DIO0',
+	'DIO1',
+	'DIO2',
+	'DIO3',
+	'DIO4',
+	'DIO5',
+	'DIO6',
+	'DIO7',
+	'DIO8',
+	'DIO9',
+	'DIO10',
+	'DIO11',
+	'DIO12',
+
+	# Analog-to-Digital Input Pins
+	'AD0',
+	'AD1',
+	'AD2',
+	'AD3',
+
+	# Named Pins
+	'VCC',
+	'DOUT',		# UART data out
+	'DIN',		# UART data in
+	'CONFIG',
+	'RESET',
+	'RSSI',		# RX Signal Strength Indicator
+	'PWM',
+	'DTR',
+	'SLEEP_RQ',	# Pin Sleep Control Line
+	'GND',
+	'CTS',		# Clear-to-Send Flow Control
+			# CTS, if enabled, is an output.
+	'ON',		# module status indicator
+	'SLEEP',	# module status indicator
+	'VREF',
+	'ASSOC',	# Associated Indicator
+	'RTS',		# Request-to-Send Flow Control
+			# RTS, if enabled, is an input.
+	'COMM',		# Commissioning Button
+)
+
+
+# pin 8 is reserved and not listed
+PIN_NAMES_TO_NUMBERS = {
+	# Digital I/O Pins
+	PINS.DIO0:	20,
+	PINS.DIO1:	19,
+	PINS.DIO2:	18,
+	PINS.DIO3:	17,
+	PINS.DIO4:	11,
+	PINS.DIO5:	15,
+	PINS.DIO6:	16,
+	PINS.DIO7:	12,
+	PINS.DIO8:	9,
+	PINS.DIO9:	13,
+	PINS.DIO10:	6,
+	PINS.DIO11:	7,
+	PINS.DIO12:	4,
+
+	# Analog-to-Digital Input Pins
+	PINS.AD0:	20,
+	PINS.AD1:	19,
+	PINS.AD2:	18,
+	PINS.AD3:	17,
+
+	# Named Pins
+	PINS.VCC:	1,
+	PINS.DOUT:	2,
+	PINS.DIN:	3,
+	PINS.CONFIG:	3,
+	PINS.RESET:	5,
+	PINS.RSSI:	6,
+	PINS.PWM:	6,
+	PINS.DTR:	9,
+	PINS.SLEEP_RQ:	9,
+	PINS.GND:	10,
+	PINS.CTS:	12,
+	PINS.ON:	13,
+	PINS.SLEEP:	13,
+	PINS.VREF:	14,
+	PINS.ASSOC:	15,
+	PINS.RTS:	16,
+	PINS.COMM:	20,
+}
