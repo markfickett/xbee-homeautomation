b6940dff935f87eb94de209f622408ce2a9ae725
Mark Fickett <mark.fickett@gmail.com>
2012-08-12 13:59:47 -0400

    Move picking a serial device and creating an Xbee object to Util.

--- xh.py
+++ xh.py
@@ -15,31 +15,8 @@ import xh
 from xh.protocol import Command, NodeDiscover, Data
 from xh.deps import serial, xbee
 
-from serial.tools import list_ports
-EXCLUDE_DEVICES = set(['Bluetooth', '-COM']) # ignore in finding Serial ports
-
 DATA_LOG_FILE = 'datalog.csv'
 log.info('also logging CSV to %s' % DATA_LOG_FILE)
-
-def getSerialCandidates():
-	candidates = [d[0] for d in list_ports.comports()
-		if all([e not in d[0] for e in EXCLUDE_DEVICES])]
-	if not candidates:
-		raise RuntimeError('No candidates for serial devices found.')
-	return candidates
-
-def pickSerialDevice():
-	serialDevices = getSerialCandidates()
-	log.info('Found serial devices: %s' % serialDevices)
-	if len(serialDevices) > 1:
-		print 'Select serial device:'
-		for i, device in enumerate(serialDevices):
-			print '%d\t%s' % (i, device)
-		iStr = raw_input('Number: ')
-		i = int(iStr)
-	else:
-		i = 0
-	return serialDevices[i]
 
 log.info('started')
 log.info('Type control-C to exit.')
@@ -59,12 +36,9 @@ def logData(rawData):
 	except:
 		log.error('error handling data: %s' % rawData, exc_info=True)
 
-try:
-	xb = None
-	serialDevice = pickSerialDevice()
-	s = serial.Serial(serialDevice, xh.Config.SERIAL_BAUD)
-	xb = xbee.ZigBee(s, callback=logData)
-	log.info('Created XBee object.')
+
+with xh.Util.InitializedXbee(callback=logData) as xb:
+	"""
 	for cmd in (
 		Command(Command.NAME.MY),
 		Command(Command.NAME.ID),
@@ -80,14 +54,8 @@ try:
 			dest=0x13a200408cca0e),
 	):
 		cmd.send(xb)
+	"""
 	while True:
 		time.sleep(0.02)
-except KeyboardInterrupt as e:
-	log.info('Got ^C.')
-except Exception as e:
-	log.error(e.message, exc_info=True)
-finally:
-	if xb:
-		xb.halt()
 
 log.info('exiting')
