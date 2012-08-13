3913de5448c2c52e4157dc6c2bee43c5dfad1220
Mark Fickett <mark.fickett@gmail.com>
2012-08-12 23:30:21 -0400

    Reorder import for Sample to be used by a Command.

--- xh/protocol/__init__.py
+++ xh/protocol/__init__.py
@@ -4,16 +4,18 @@ from Registry import Registry
 
 from Frame import Frame, FrameRegistry
 
+from Data import Data, Sample
+
 from NodeId import NodeId
 
 from Command import Command, CommandRegistry
 from ConfigureIoPin import ConfigureIoPin
 from EncryptionEnable import EncryptionEnable
+from InputSample import InputSample
 from InputVolts import InputVolts
 from NodeDiscover import NodeDiscover
 from PullUpResistor import PullUpResistor
 from SampleRate import SampleRate
-
-from Data import Data
+from VoltageSupplyThreshold import VoltageSupplyThreshold
 
 from Parse import ParseFromDict
