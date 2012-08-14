90bc0d7128292e4755fa99a17c95ebbb84349ae8
Mark Fickett <mark.fickett@gmail.com>
2012-08-14 00:20:09 -0400

    Try to import all deps before raising exception.

--- xh/deps/__init__.py
+++ xh/deps/__init__.py
@@ -5,22 +5,29 @@ Verify and import third-party dependencies.
 import logging
 log = logging.getLogger('xh.deps')
 
+failedImports = []
+
 try:
 	import serial
 except ImportError as e:
-	log.error('Required: pySerial >=2.6 '
-		+ 'from http://pyserial.sourceforge.net/')
-	raise e
+	failedImports.append(
+		('pySerial >=2.6 from http://pyserial.sourceforge.net/', e))
 
 try:
 	import xbee
 except ImportError as e:
-	log.error('Required: python-xbee '
-		+ 'from http://code.google.com/p/python-xbee/')
-	raise e
+	failedImports.append(
+		('python-xbee from http://code.google.com/p/python-xbee/', e))
 
 try:
 	from enum import Enum
 except ImportError as e:
-	log.error('Required: enum from http://pypi.python.org/pypi/enum/')
+	failedImports.append(('enum from http://pypi.python.org/pypi/enum/', e))
+
+if failedImports:
+	msg = 'Unable to import required dependencies:'
+	for description, e in failedImports:
+		msg += '\n\t%s: requires %s' % (e.message, description)
+	log.error(msg)
 	raise e
+
