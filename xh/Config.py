17790608c84f67cd9abbb4a2a1127f30ed3964ab
Mark Fickett <mark.fickett@gmail.com>
2012-08-20 21:56:57 -0400

    Add plugin directory and extension to Config.

--- xh/Config.py
+++ xh/Config.py
@@ -3,6 +3,8 @@ Configuration constants (shared, not expected to change).
 """
 
 SERIAL_BAUD = 9600
+PLUGIN_DIR = 'plugins'
+PLUGIN_INFO_EXTENSION = 'xh-plugin-info'
 
 # Link encryption key, etc. Values not to check into version control.
 from SecretConfig import *
